7481800d8f7d146e13641df833ba43bdb02ff249
godsunika <103250935+godsunika@users.noreply.github.com>
2022-10-06 15:00:22 +0700

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from '~/reportWebVitals';
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ThemeProvider, createTheme, responsiveFontSizes } from "@mui/material/styles";
+
+import '~/index.css';
+import App from '~/App';
+
+const root        = ReactDOM.createRoot(document.getElementById('root'));
+const queryClient = new QueryClient();
+
+let theme = createTheme({
+  typography: {
+    fontFamily: 'sans-serif',
+  }
+});
+
+theme = responsiveFontSizes(theme);
+
+root.render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}> 
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
