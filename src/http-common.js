9833f58c506d1cfb65ad1f0be9907cc3262d2ebf
godsunika <103250935+godsunika@users.noreply.github.com>
2022-10-12 15:51:46 +0700

    fix api

--- src/http-common.js
+++ src/http-common.js
@@ -1,6 +1,10 @@
+import * as dotenv from 'dotenv'
 import axios from "axios";
+
+dotenv.config()
+
 export default axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: process.env.IP_SERVICE,
   headers: {
     "Content-type": "application/json"
   }
